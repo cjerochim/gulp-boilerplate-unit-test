cace555b11efebb3b737c5df080da4e0c32aea55
Jerochim <cjerochim@chrisdaledtimac.stwgroup.net.au>
2014-12-04 08:45:53 +1100

    Minor tweaks to Assemble and added notify

--- gulpfile.js
+++ gulpfile.js
@@ -8,69 +8,86 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     mocha = require('gulp-mocha'),
     browserify = require('gulp-browserify'),
+    notify = require('gulp-notify'),
     config = require('./config');
-
-
-
-
-gulp.task('default', function() {});
 
 
 /**
  * css
  * */
-
 gulp.task('css', ['css:sass']);
+
 
 gulp.task('css:sass', function() {
     return gulp.src(config.sass.src)
         .pipe(sass())
-        .pipe(gulp.dest(config.sass.dest));
+        .pipe(gulp.dest(config.sass.dest))
+        .pipe(notify({
+            message: 'CSS Complete'
+        }));
 });
 
+
 /**
- * Remove
+ * Remove Entire build
  */
-gulp.task('remove', ['remove:clean']);
-
-gulp.task('remove:clean', function() {
+gulp.task('remove', function() {
    return gulp.src(config.clean.dest, {read: false})
-       .pipe(clean());
+       .pipe(clean())
+       .pipe(notify({
+           message: 'Remove Complete'
+       }));
 });
 
 
 
 /**
- * HTML
+ * HTML / Generate html and ensure it's pretty
  */
 gulp.task('html', ['html:assemble']);
 
 gulp.task('html:assemble', function() {
-    return gulp.src(config.assemble.paths.src)
+   return gulp.src(config.assemble.paths.src)
         .pipe(assemble(config.assemble.properties))
         .pipe(htmlTidy({
             indent: true
         }))
-        .pipe(gulp.dest(config.assemble.paths.dest));
+        .pipe(gulp.dest(config.assemble.paths.dest))
+       .pipe(notify({
+           message: 'Assemble Complete'
+       }));
 });
 
 
 
+
+/****************
+ * JS
+ *****************/
 gulp.task('js', ['js:browserify']);
 gulp.task('test', ['js:test']);
 
+/**
+ * Unit Testing JS files
+ */
 gulp.task('js:test', function() {
     return gulp.src(config.mocha.paths.src)
         .pipe(mocha());
 });
 
+
+/**
+ * Bundle JS files into modules
+ */
 gulp.task('js:browserify', function() {
     return gulp.src(config.browserify.paths.src)
         .pipe(browserify(config.browserify.options))
         .pipe(gulp.dest(config.browserify.paths.dest));
 });
 
-
+/**
+ *  Local hosting / reload of page
+ **/
 gulp.task('browser-sync', function() {
     browserSync({
       server: {
@@ -80,13 +97,17 @@ gulp.task('browser-sync', function() {
 });
 
 
-gulp.task('default', ['browser-sync'], function() {
+/**
+ *  Watch files and update browser if required.
+ **/
+gulp.task('default', ['browser-sync', 'test', 'build'], function() {
     gulp.watch('./src/scss/**/*.scss', [ 'css', browserSync.reload]);
     gulp.watch('./src/templates/**/*.hbs', ['html', browserSync.reload]);
     gulp.watch('./src/js/app/**/*.js', ['js', browserSync.reload]);
 
-    //No need to reload page
+    //No need to reload page for unit tests
     gulp.watch('./src/js/tests/**/*.js', ['test']);
 });
 
-gulp.task('build', [ 'css', 'html']);
+
+gulp.task('build', [ 'css', 'js']);
--- src/js/app/main.js
+++ src/js/app/main.js
@@ -1,5 +1,7 @@
-var Sample = require('./sample').sample;
+var Sample = require('./sample').sample,
+    specificityGraph = require('specificity-graph')
 
 var item = new Sample();
 item.test();
+
 
